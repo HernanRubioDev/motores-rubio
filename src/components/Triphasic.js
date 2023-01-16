884ae2d85066057ff9bdb94b1f64e6b85cf7f53d
Hernan Rubio <hernan.andres.rubio@gmail.com>
2023-01-16 11:34:14 -0300

    inputs control added

--- src/components/Triphasic.js
+++ src/components/Triphasic.js
@@ -1,15 +1,36 @@
 import { useState } from 'react';
 import useActive from '../hooks/useActive';
+import useForm from '../hooks/useForm';
 
 const Triphasic = ()=>{
+
+  const initialForm = {
+    brand:'',
+    rpm:'',
+    minorDim:'',
+    model:'',
+    hp:'',
+    majorDim:'',
+    owner:'',
+    slots:'',
+    large:'',
+    startWire:'',
+    voltage:'',
+    connection:'',
+    windingType:'',
+    observations:''
+  }
+
   const [activeBtn, setActiveBtn] = useState('triphasic-left__link');
   const {changeActive} = useActive();
+  const {handleChange, form} = useForm(initialForm);
 
   const handleActive = (e)=>{
     changeActive(e, activeBtn, setActiveBtn);
   }
   return(
-  <section className="d-flex container-fluid justify-content-center align-items-center h-100 w-100 bg-body-secondary">
+  <section className="d-flex flex-column container-fluid justify-content-evenly align-items-center h-100 w-100 bg-body-secondary">
+    <h3 className='fs-2 fw-bold text-secondary'>Nuevo Trifásico</h3>
     <div className="w-75 h-75 border border-secondary border-top-0 bg-white d-flex flex-column box-shadow">
       <nav className="w-100 d-flex flex-nowrap flex-row justify-content-evenly list-group">
       <ul className="nav nav-pills w-100">  
@@ -28,31 +49,31 @@ const Triphasic = ()=>{
         <div id="triphasic-left" className="login-form__div w-100 h-100 d-flex flex-column justify-content-evenly align-items-center align-content-center">
           <div className='col-11 d-flex flex-wrap h-50 border border-secondary justify-content-evenly '>
               <div className="d-flex align-items-center m-2 w-25">
-                <input type="text" className="form-control border-secondary"placeholder="Marca"/>
+                <input onChange={(e)=>handleChange(e)} type="text" className="form-control border-secondary"placeholder="Marca" name='brand' value={form.brand}/>
               </div>
               <div className="d-flex align-items-center m-2 w-25">
-                <input type="text" className="form-control border-secondary"placeholder="Rpm"/>
+                <input onChange={(e)=>handleChange(e)} type="text" className="form-control border-secondary"placeholder="Rpm" name='rpm' value={form.rpm}/>
               </div>
               <div className="d-flex align-items-center m-2 w-25">
-                <input type="text" className="form-control border-secondary"placeholder="Dim. Mayor"/>
+                <input onChange={(e)=>handleChange(e)} type="text" className="form-control border-secondary"placeholder="Dim. Mayor" name='majorDim' value={form.majorDim}/>
               </div>
               <div className="d-flex align-items-center m-2 w-25">
-                <input type="text" className="form-control border-secondary"placeholder="Modelo"/>
+                <input onChange={(e)=>handleChange(e)} type="text" className="form-control border-secondary"placeholder="Modelo" name='model' value={form.model}/>
               </div>
               <div className="d-flex align-items-center m-2 w-25">
-                <input type="text" className="form-control border-secondary"placeholder="Hp"/>
+                <input onChange={(e)=>handleChange(e)} type="text" className="form-control border-secondary"placeholder="Hp" name='hp' value={form.hp}/>
               </div>
               <div className="d-flex align-items-center m-2 w-25">
-                <input type="text" className="form-control border-secondary"placeholder="Dim. Menor"/>
+                <input onChange={(e)=>handleChange(e)} type="text" className="form-control border-secondary"placeholder="Dim. Menor" name='minorDim' value={form.minorDim}/>
               </div>
               <div className="d-flex align-items-center m-2 w-25">
-                <input type="text" className="form-control border-secondary"placeholder="Dueño"/>
+                <input onChange={(e)=>handleChange(e)} type="text" className="form-control border-secondary"placeholder="Dueño" name='owner' value={form.owner}/>
               </div>
               <div className="d-flex align-items-center m-2 w-25">
-                <input type="text" className="form-control border-secondary"placeholder="Ranura"/>
+                <input onChange={(e)=>handleChange(e)} type="text" className="form-control border-secondary"placeholder="Ranura" name='slots' value={form.slots}/>
               </div>
               <div className="d-flex align-items-center m-2 w-25">
-                <input type="text" className="form-control border-secondary"placeholder="Largo"/>
+                <input onChange={(e)=>handleChange(e)} type="text" className="form-control border-secondary"placeholder="Largo" name='large' value={form.large}/>
               </div>
           </div>
         </div>
@@ -60,24 +81,24 @@ const Triphasic = ()=>{
           <div className="w-75 border border-secondary d-flex align-items-center justify-content-evenly h-50">
             <div className=' col-11 d-flex flex-column border border-secondary w-25 p-2'>
               <div className="d-flex align-items-center m-2">
-                <input type="text" className="form-control border-secondary"placeholder="Paso"/>
+                <input onChange={(e)=>handleChange(e)} type="text" className="form-control border-secondary"placeholder="Paso" name='steps' value={form.steps}/>
               </div>
               <div className="d-flex align-items-center m-2">
-                <input type="text" className="form-control border-secondary"placeholder="Vuelta"/>
+                <input onChange={(e)=>handleChange(e)} type="text" className="form-control border-secondary"placeholder="Vuelta" name='laps' value={form.laps}/>
               </div>
               <div className="d-flex align-items-center m-2">
-                <input type="text" className="form-control border-secondary"placeholder="Alambre"/>
+                <input onChange={(e)=>handleChange(e)} type="text" className="form-control border-secondary"placeholder="Alambre" name='wire' value={form.wire}/>
               </div>
             </div>
             <div className=' col-11 d-flex flex-column border border-secondary w-25 p-2'>
               <div className="d-flex align-items-center m-2">
-                <input type="text" className="form-control border-secondary"placeholder="Voltaje"/>
+                <input onChange={(e)=>handleChange(e)} type="text" className="form-control border-secondary"placeholder="Voltaje" name='voltage' value={form.voltage}/>
               </div>
               <div className="d-flex align-items-center m-2">
-                <input type="text" className="form-control border-secondary"placeholder="Conexión"/>
+                <input onChange={(e)=>handleChange(e)} type="text" className="form-control border-secondary"placeholder="Conexión" name='connection' value={form.connection}/>
               </div>
               <div className="d-flex align-items-center m-2">
-                <input type="text" className="form-control border-secondary"placeholder="Tipo de Bobinado"/>
+                <input onChange={(e)=>handleChange(e)} type="text" className="form-control border-secondary"placeholder="Tipo de Bobinado" name='windingType' value={form.windingType}/>
               </div>
             </div>
           </div>
@@ -85,7 +106,7 @@ const Triphasic = ()=>{
         <div id="triphasic-right" className="login-form__div w-100 h-100 d-flex justify-content-center align-items-center">
           <div className="w-75 d-flex flex-column align-items-center justify-content-evenly h-100">
             <div className="form-floating w-75 h-75">
-              <textarea className="form-control border-secondary h-100" placeholder="Leave a comment here" id="floatingTextarea"></textarea>
+              <textarea onChange={(e)=>handleChange(e)} className="form-control border-secondary h-100" placeholder="Leave a comment here" id="floatingTextarea" name='observations' value={form.observations}></textarea>
               <label htmlFor="floatingTextarea">Observaciones</label>
             </div>
             <button type="button" className="btn btn-primary w-25">Guardar</button>
--- src/components/Washer.js
+++ src/components/Washer.js
@@ -1,15 +1,51 @@
 import { useState } from 'react';
 import useActive from '../hooks/useActive';
+import useForm from '../hooks/useForm';
 
 const Washer = ()=>{
+
+  const initialForm = {
+    brand:'',
+    rpm:'',
+    minorDim:'',
+    model:'',
+    hp:'',
+    majorDim:'',
+    owner:'',
+    slots:'',
+    large:'',
+    centWorksteps:'',
+    centWorklaps:'',
+    centWorkwire:'',
+    centStartSteps:'',
+    centStartLaps:'',
+    centStartWire:'',
+    centVoltage:'',
+    centConnection:'',
+    centwindingType:'',
+    centCapacity:'',
+    washWorkfSteps:'',
+    washWorkfLaps:'',
+    washWorkfWire:'',
+    washWorksSteps:'',
+    washWorksLaps:'',
+    washWorksWire:'',
+    washStartSteps:'',
+    washStartLaps:'',
+    washStartWire:'',
+    observations:''
+  }
+
   const [activeBtn, setActiveBtn] = useState('washer-left__link');
   const {changeActive} = useActive();
+  const {handleChange, form} = useForm(initialForm);
 
   const handleActive = (e)=>{
     changeActive(e, activeBtn, setActiveBtn);
   }
 return(
-<section className="d-flex container-fluid justify-content-center align-items-center h-100 w-100 bg-body-secondary ">
+<section className="d-flex flex-column container-fluid justify-content-evenly align-items-center h-100 w-100 bg-body-secondary ">
+    <h3 className='fs-2 fw-bold text-secondary'>Nuevo Lavarropas Automático</h3>
     <div className="w-75 h-75 border border-secondary border-top-0 bg-white d-flex flex-column box-shadow">
       <nav className="w-100 d-flex flex-nowrap flex-row justify-content-evenly list-group">
       <ul className="nav nav-pills w-100">  
@@ -28,73 +64,112 @@ return(
         <div id="washer-left" className="login-form__div w-100 h-100 d-flex flex-column justify-content-evenly align-items-center align-content-center">
           <div className='col-11 d-flex flex-wrap h-50 border border-secondary justify-content-evenly '>
               <div className="d-flex align-items-center m-2 w-25">
-                <input type="text" className="form-control border-secondary"placeholder="Marca"/>
+                <input onChange={(e)=>handleChange(e)} type="text" className="form-control border-secondary"placeholder="Marca" name='brand' value={form.brand}/>
               </div>
               <div className="d-flex align-items-center m-2 w-25">
-                <input type="text" className="form-control border-secondary"placeholder="Rpm"/>
+                <input onChange={(e)=>handleChange(e)} type="text" className="form-control border-secondary"placeholder="Rpm" name='rpm' value={form.rpm}/>
               </div>
               <div className="d-flex align-items-center m-2 w-25">
-                <input type="text" className="form-control border-secondary"placeholder="Dim. Mayor"/>
+                <input onChange={(e)=>handleChange(e)} type="text" className="form-control border-secondary"placeholder="Dim. Mayor" name='majorDim' value={form.majorDim}/>
               </div>
               <div className="d-flex align-items-center m-2 w-25">
-                <input type="text" className="form-control border-secondary"placeholder="Modelo"/>
+                <input onChange={(e)=>handleChange(e)} type="text" className="form-control border-secondary"placeholder="Modelo" name='model' value={form.model}/>
               </div>
               <div className="d-flex align-items-center m-2 w-25">
-                <input type="text" className="form-control border-secondary"placeholder="Hp"/>
+                <input onChange={(e)=>handleChange(e)} type="text" className="form-control border-secondary"placeholder="Hp" name='hp' value={form.hp}/>
               </div>
               <div className="d-flex align-items-center m-2 w-25">
-                <input type="text" className="form-control border-secondary"placeholder="Dim. Menor"/>
+                <input onChange={(e)=>handleChange(e)} type="text" className="form-control border-secondary"placeholder="Dim. Menor" name='minorDim' value={form.minorDim}/>
               </div>
               <div className="d-flex align-items-center m-2 w-25">
-                <input type="text" className="form-control border-secondary"placeholder="Dueño"/>
+                <input onChange={(e)=>handleChange(e)} type="text" className="form-control border-secondary"placeholder="Dueño" name='owner' value={form.owner}/>
               </div>
               <div className="d-flex align-items-center m-2 w-25">
-                <input type="text" className="form-control border-secondary"placeholder="Ranura"/>
+                <input onChange={(e)=>handleChange(e)} type="text" className="form-control border-secondary"placeholder="Ranura" name='slots' value={form.slots}/>
               </div>
               <div className="d-flex align-items-center m-2 w-25">
-                <input type="text" className="form-control border-secondary"placeholder="Largo"/>
+                <input onChange={(e)=>handleChange(e)} type="text" className="form-control border-secondary"placeholder="Largo" name='large' value={form.large}/>
               </div>
           </div>
         </div>
-        <div id="washer-center" className="login-form__div w-100 h-100 d-flex justify-content-evenly align-items-center">
-          <div className="w-75 border border-secondary d-flex align-items-center justify-content-evenly h-75 position-relative">
+        <div id="washer-center" className="login-form__div w-100 h-100 d-flex flex-column justify-content-evenly align-items-center">
+          <div className="w-75 border border-secondary d-flex align-items-center justify-content-evenly position-relative">
             <h6 className="position-absolute top-0 start-50 translate-middle bg-white">Centrifugado</h6>
             <div className=' col-11 d-flex flex-column border border-secondary w-25 position-relative p-2'>
             <p className="mono-lable fw-semibold position-absolute start-50 bg-white translate-middle">Trabajo</p>
             <div className="d-flex align-items-center m-2">
-              <input type="text" className="form-control border-secondary"placeholder="Paso"/>
+              <input onChange={(e)=>handleChange(e)} type="text" className="form-control border-secondary"placeholder="Paso" name='centWorksteps' value={form.centWorksteps}/>
             </div>
             <div className="d-flex align-items-center m-2">
-              <input type="text" className="form-control border-secondary"placeholder="Vuelta"/>
+              <input onChange={(e)=>handleChange(e)} type="text" className="form-control border-secondary"placeholder="Vuelta" name='centWorklaps' value={form.centWorklaps}/>
             </div>
             <div className="d-flex align-items-center m-2">
-              <input type="text" className="form-control border-secondary"placeholder="Alambre"/>
+              <input onChange={(e)=>handleChange(e)} type="text" className="form-control border-secondary"placeholder="Alambre" name='centWorkwire' value={form.centWorkwire}/>
             </div>
           </div>
           <div className=' col-11 d-flex flex-column border border-secondary w-25 position-relative p-2'>
             <p className="mono-lable fw-semibold position-absolute start-50 bg-white translate-middle">Arranque</p>
             <div className="d-flex align-items-center m-2">
-              <input type="text" className="form-control border-secondary"placeholder="Paso"/>
+              <input onChange={(e)=>handleChange(e)} type="text" className="form-control border-secondary"placeholder="Paso" name='centStartSteps' value={form.centStartSteps}/>
             </div>
             <div className="d-flex align-items-center m-2">
-              <input type="text" className="form-control border-secondary"placeholder="Vuelta"/>
+              <input onChange={(e)=>handleChange(e)} type="text" className="form-control border-secondary"placeholder="Vuelta" name='centStartLaps' value={form.centStartLaps}/>
             </div>
             <div className="d-flex align-items-center m-2">
-              <input type="text" className="form-control border-secondary"placeholder="Alambre"/>
+              <input onChange={(e)=>handleChange(e)} type="text" className="form-control border-secondary"placeholder="Alambre" name='centStartWire' value={form.centStartWire}/>
             </div>
           </div>
           <div className='d-flex  flex-wrap w-25 border border-secondary p-2'>
             <div className="d-flex align-items-center m-2">
-              <input type="text" className="form-control border-secondary"placeholder="Voltaje"/>
+              <input onChange={(e)=>handleChange(e)} type="text" className="form-control border-secondary"placeholder="Voltaje" name='centVoltage' value={form.centVoltage}/>
             </div>
             <div className="d-flex align-items-center m-2">
-              <input type="text" className="form-control border-secondary"placeholder="Conexion"/>
+              <input onChange={(e)=>handleChange(e)} type="text" className="form-control border-secondary"placeholder="Conexion" name='centConnection' value={form.centConnection}/>
             </div>
             <div className="d-flex align-items-center m-2">
-              <input type="text" className="form-control border-secondary"placeholder="Tipo de Bobinado"/>
+              <input onChange={(e)=>handleChange(e)} type="text" className="form-control border-secondary"placeholder="Tipo de Bobinado" name='centwindingType' value={form.centwindingType}/>
             </div>
             <div className="d-flex flex-wrap align-items-center m-2">
-              <input type="text" className="form-control border-secondary"placeholder="Capacidad"/>
+              <input onChange={(e)=>handleChange(e)} type="text" className="form-control border-secondary"placeholder="Capacidad" name='centCapacity' value={form.centCapacity}/>
+            </div>
+          </div>
+          </div>
+          <div className="w-75 border border-secondary d-flex align-items-center justify-content-evenly position-relative h-50">
+            <h6 className="position-absolute top-0 start-50 translate-middle bg-white">Lavado</h6>
+            <div className=' col-11 d-flex flex-column border border-secondary w-25 position-relative p-2'>
+            <p className="mono-lable fw-semibold position-absolute start-50 bg-white translate-middle">Trabajo</p>
+            <div className="d-flex align-items-center m-2">
+              <input onChange={(e)=>handleChange(e)} type="text" className="form-control border-secondary"placeholder="Paso" name='washWorkfSteps' value={form.washWorkfSteps}/>
+            </div>
+            <div className="d-flex align-items-center m-2">
+              <input onChange={(e)=>handleChange(e)} type="text" className="form-control border-secondary"placeholder="Vueltas" name='washWorkfLaps' value={form.washWorkfLaps}/>
+            </div>
+            <div className="d-flex align-items-center m-2">
+              <input onChange={(e)=>handleChange(e)} type="text" className="form-control border-secondary"placeholder="Alambre" name='washWorkfWire' value={form.washWorkfWire}/>
+            </div>
+          </div>
+          <div className=' col-11 d-flex flex-column border border-secondary w-25 position-relative p-2'>
+            <p className="mono-lable fw-semibold position-absolute start-50 bg-white translate-middle">Trabajo</p>
+            <div className="d-flex align-items-center m-2">
+              <input onChange={(e)=>handleChange(e)} type="text" className="form-control border-secondary"placeholder="Paso" name='washWorksSteps' value={form.washWorksSteps}/>
+            </div>
+            <div className="d-flex align-items-center m-2">
+              <input onChange={(e)=>handleChange(e)} type="text" className="form-control border-secondary"placeholder="Vueltas" name='washWorksLaps' value={form.washWorksLaps}/>
+            </div>
+            <div className="d-flex align-items-center m-2">
+              <input onChange={(e)=>handleChange(e)} type="text" className="form-control border-secondary"placeholder="Alambre" name='washWorksWire' value={form.washWorksWire}/>
+            </div>
+          </div>
+          <div className=' col-11 d-flex flex-column border border-secondary w-25 position-relative p-2'>
+            <p className="mono-lable fw-semibold position-absolute start-50 bg-white translate-middle">Arranque</p>
+            <div className="d-flex align-items-center m-2">
+              <input onChange={(e)=>handleChange(e)} type="text" className="form-control border-secondary"placeholder="Paso" name='washStartSteps' value={form.washStartSteps}/>
+            </div>
+            <div className="d-flex align-items-center m-2">
+              <input onChange={(e)=>handleChange(e)} type="text" className="form-control border-secondary"placeholder="Vueltas" name='washStartLaps' value={form.washStartLaps}/>
+            </div>
+            <div className="d-flex align-items-center m-2">
+              <input onChange={(e)=>handleChange(e)} type="text" className="form-control border-secondary"placeholder="Alambre" name='washStartWire' value={form.washStartWire}/>
             </div>
           </div>
           </div>
@@ -102,8 +177,8 @@ return(
         <div id="washer-right" className="login-form__div w-100 h-100 d-flex justify-content-center align-items-center">
           <div className="w-75 d-flex flex-column align-items-center justify-content-evenly h-100">
             <div className="form-floating w-75 h-75">
-              <textarea className="form-control border-secondary h-100" placeholder="Leave a comment here" id="floatingTextarea"></textarea>
-              <label for="floatingTextarea">Observaciones</label>
+              <textarea onChange={(e)=>handleChange(e)} className="form-control border-secondary h-100" placeholder="Leave a comment here" id="floatingTextarea" name='observations' value={form.observations}></textarea>
+              <label htmlFor="floatingTextarea">Observaciones</label>
             </div>
             <button type="button" className="btn btn-primary w-25">Guardar</button>
           </div>
