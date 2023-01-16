9a846337f9a4fb5f953d1823e8274440c6bd0fff
Hernan Rubio <hernan.andres.rubio@gmail.com>
2023-01-16 14:20:10 -0300

    more input control added

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,15 @@
 import { Link } from "react-router-dom";
+import useForm from '../hooks/useForm';
 
 const Login = ()=>{
+
+  const initialForm={
+    username:'',
+    password:'',
+  }
+
+  const {handleChange, form} = useForm(initialForm);
+
   return(
     <section className="d-flex container-fluid justify-content-center align-items-center h-100 w-100 bg-body-secondary">
       <form className="border container-fluid p-3 w-sm-100 col-sm-6 col-md-5 col-lg-4 col-xl-3 bg-white rounded-1">
@@ -10,7 +19,7 @@ const Login = ()=>{
           <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" className="bi bi-person" viewBox="0 0 16 16">
             <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z"/>
           </svg>
-            <input type="email" className="form-control ms-2" id="userLogin" placeholder="Usuario"/>
+            <input onChange={(e)=>handleChange(e)} type="text" className="form-control ms-2" id="userLogin" placeholder="Usuario" name='username' value={form.username}/>
           </div>
           <div id="userLogin" className="form-text text-danger text-end">Usuario incorecto</div>
         </div>
@@ -20,7 +29,7 @@ const Login = ()=>{
               <path d="M0 8a4 4 0 0 1 7.465-2H14a.5.5 0 0 1 .354.146l1.5 1.5a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0L13 9.207l-.646.647a.5.5 0 0 1-.708 0L11 9.207l-.646.647a.5.5 0 0 1-.708 0L9 9.207l-.646.647A.5.5 0 0 1 8 10h-.535A4 4 0 0 1 0 8zm4-3a3 3 0 1 0 2.712 4.285A.5.5 0 0 1 7.163 9h.63l.853-.854a.5.5 0 0 1 .708 0l.646.647.646-.647a.5.5 0 0 1 .708 0l.646.647.646-.647a.5.5 0 0 1 .708 0l.646.647.793-.793-1-1h-6.63a.5.5 0 0 1-.451-.285A3 3 0 0 0 4 5z"/>
               <path d="M4 8a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
             </svg>
-            <input type="password" className="form-control ms-2" id="passwordLogin" placeholder="Contraseña"/>
+            <input onChange={(e)=>handleChange(e)} type="password" className="form-control ms-2" id="passwordLogin" placeholder="Contraseña" name='password' value={form.password}/>
           </div>
           <div id="passwordLogin" className="form-text text-danger text-end">La contraseña es demaciado corta</div>
         </div>
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,51 +1,69 @@
+import useForm from "../hooks/useForm";
+
 const Search = ()=>{
+
+  const initialForm = {
+    brand:'',
+    rpm:'',
+    minorDim:'',
+    model:'',
+    hp:'',
+    majorDim:'',
+    owner:'',
+    slots:'',
+    large:'',
+    motorType:'',
+    startType:'',
+  }
+
+  const {handleChange, form} = useForm(initialForm);
   return(
     <section className="d-flex p-3 container-fluid justify-content-center align-items-center h-100 w-100 bg-body-secondary">
       <div className="w-75 h-100 border border-secondary bg-white d-flex flex-column align-items-center justify-content-between box-shadow">
         <form className=" w-100 h-75 d-flex align-items-center justify-content-evenly flex-wrap overflow-hidden px-3">
           <div className="d-flex h-50 w-75 border border-secondary border-end-0 flex-wrap align-items-center justify-content-evenly">
               <div className="d-flex align-items-center m-2 w-25">
-                <input type="text" className="form-control border-secondary"placeholder="Marca"/>
+                <input onChange={(e)=>handleChange(e)} type="text" className="form-control border-secondary"placeholder="Marca" name='brand' value={form.brand}/>
               </div>
               <div className="d-flex align-items-center m-2 w-25">
-                <input type="text" className="form-control border-secondary"placeholder="Rpm"/>
+                <input onChange={(e)=>handleChange(e)} type="text" className="form-control border-secondary"placeholder="Rpm" name='rpm' value={form.rpm}/>
               </div>
               <div className="d-flex align-items-center m-2 w-25">
-                <input type="text" className="form-control border-secondary"placeholder="Dim. Mayor"/>
+                <input onChange={(e)=>handleChange(e)} type="text" className="form-control border-secondary"placeholder="Dim. Mayor" name='majorDim' value={form.majorDim}/>
               </div>
               <div className="d-flex align-items-center m-2 w-25">
-                <input type="text" className="form-control border-secondary"placeholder="Modelo"/>
+                <input onChange={(e)=>handleChange(e)} type="text" className="form-control border-secondary"placeholder="Modelo" name='model' value={form.model}/>
               </div>
               <div className="d-flex align-items-center m-2 w-25">
-                <input type="text" className="form-control border-secondary"placeholder="Hp"/>
+                <input onChange={(e)=>handleChange(e)} type="text" className="form-control border-secondary"placeholder="Hp" name='hp' value={form.hp}/>
               </div>
               <div className="d-flex align-items-center m-2 w-25">
-                <input type="text" className="form-control border-secondary"placeholder="Dim. Menor"/>
+                <input onChange={(e)=>handleChange(e)} type="text" className="form-control border-secondary"placeholder="Dim. Menor" name='minorDim' value={form.minorDim}/>
               </div>
               <div className="d-flex align-items-center m-2 w-25">
-                <input type="text" className="form-control border-secondary"placeholder="Dueño"/>
+                <input onChange={(e)=>handleChange(e)} type="text" className="form-control border-secondary"placeholder="Dueño" name='owner' value={form.owner}/>
               </div>
               <div className="d-flex align-items-center m-2 w-25">
-                <input type="text" className="form-control border-secondary"placeholder="Ranura"/>
+                <input onChange={(e)=>handleChange(e)} type="text" className="form-control border-secondary"placeholder="Ranura" name='slots' value={form.slots}/>
               </div>
               <div className="d-flex align-items-center m-2 w-25">
-                <input type="text" className="form-control border-secondary"placeholder="Largo"/>
+                <input onChange={(e)=>handleChange(e)} type="text" className="form-control border-secondary"placeholder="Largo" name='large' value={form.large}/>
               </div>
           </div>
           <div className="border border-secondary h-50 w-25 d-flex flex-column align-items-center justify-content-evenly">
               <div className="d-flex align-items-center m-3 w-75">
-                <select className="form-select border-secondary" id="inputGroupSelect01">
-                  <option >Tipo de Motor</option>
-                  <option value="1">Monofásico</option>
-                  <option value="2">Trifásico</option>
-                  <option value="3">Lavarropas Automático</option>
+                <select onChange={(e)=>handleChange(e)} className="form-select onChange={(e)=>handleChange(e)} border-secondary" id="inputGroupSelect01" name='motorType' value={form.motorType}>
+                  <option value='' disabled>Tipo de Motor</option>
+                  <option value="monophasic">Monofásico</option>
+                  <option value="triphasic">Trifásico</option>
+                  <option value="washer">Lavarropas Automático</option>
                 </select>
               </div>
               <div className="d-flex align-items-center m-3 w-75">
-                <select className="form-select border-secondary" id="inputGroupSelect02">
-                  <option >Tipo de Arranque</option>
-                  <option value="1">Plaqueta</option>
-                  <option value="2">Condensador</option>
+                <select onChange={(e)=>handleChange(e)} className="form-select onChange={(e)=>handleChange(e)} border-secondary" id="inputGroupSelect02" name='startType' value={form.startType}>
+                  <option value='' disabled>Tipo de Arranque</option>
+                  <option value="plaqueta">Plaqueta</option>
+                  <option value="condensador">Condensador</option>
                 </select>
               </div>
           </div>
